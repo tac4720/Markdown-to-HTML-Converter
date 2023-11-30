f36a3d55854f6d104eb734847474bfcb60b2a0e7
tac4720 <138498889+tac4720@users.noreply.github.com>
2023-11-30 11:03:46 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+import markdown
+
+def main():
+    argc = len(sys.argv)
+    if argc != 5:
+        print("Usage: python markdown main.py inputfile.md outputfile.html")
+        return 1
+
+    inputfile = sys.argv[2]
+    outputfile = sys.argv[3]
+
+    with open(inputfile, 'r') as f:
+        markdown_text = f.read()
+
+    html = markdown.markdown(markdown_text)
+
+    with open(outputfile, 'w') as f:
+        f.write(html)
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
